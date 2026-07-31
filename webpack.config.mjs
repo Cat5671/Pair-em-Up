b892c99ff700e73abb3856e36a40f823c9865d2f
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2026-07-31 10:45:17 +0300

    chore: base setup builder

---
+++ webpack.config.mjs
@@ -0,0 +1,57 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+ 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/main.js',
+  
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js', // JS будет сохраняться в папку dist/js
+    clean: true,
+  },
+
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+ !
+          MiniCssExtractPlugin.loader, 
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    
+ 
+    new MiniCssExtractPlugin({
+  
+      filename: 'css/[name].css', 
+    }),
+  ],
+};
